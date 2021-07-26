213c7fefc8e908edb4540969944f760c7dac27dc
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-26 10:19:20 -0700

    added html, js and css

---
+++ index.js
@@ -0,0 +1,21 @@
+const users = [
+  { fname: "esme", lname: "sanchez", id: 1892038, isTherapist: true },
+
+  { fname: "rick", lname: "jeps", id: 1894024, isTherapist: false },
+
+  { fname: "angie", lname: "lange", id: 1982403, isTherapist: false },
+
+  { fname: "renat", lname: "gros", id: 1894039, isTherapist: true },
+
+  { fname: "bettino", lname: "yang", id: 3789204, isTherapist: true },
+];
+
+const container = document.querySelector("#users_container");
+const ul = document.createElement("ul");
+
+users.forEach(function(user) {
+    let li = document.createElement('li');
+
+    li.textContent = user.fname + ', ' + user.lname;
+    ul.appendChild(li);
+});
