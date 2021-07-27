64e8a458c6e45452aae0671a643af12cd4048ae2
aleksandar <aca.djordjevic1113@gmail.com>
2021-07-27 08:49:28 -0700

    names showing up

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const users = [
-  { fname: "esme", lname: "sanchez", id: 1892038, isTherapist: true },
 
-  { fname: "rick", lname: "jeps", id: 1894024, isTherapist: false },
+window.addEventListener("DOMContentLoaded", (event) => {
+  const users = [
+    { fname: "esme", lname: "sanchez", id: 1892038, isTherapist: true },
 
-  { fname: "angie", lname: "lange", id: 1982403, isTherapist: false },
+    { fname: "rick", lname: "jeps", id: 1894024, isTherapist: false },
 
-  { fname: "renat", lname: "gros", id: 1894039, isTherapist: true },
+    { fname: "angie", lname: "lange", id: 1982403, isTherapist: false },
 
-  { fname: "bettino", lname: "yang", id: 3789204, isTherapist: true },
-];
+    { fname: "renat", lname: "gros", id: 1894039, isTherapist: true },
 
-const container = document.querySelector("#users_container");
-const ul = document.createElement("ul");
+    { fname: "bettino", lname: "yang", id: 3789204, isTherapist: true },
+  ];
 
-users.forEach(function(user) {
-    let li = document.createElement('li');
+  const container = document.getElementById("container");
+  const ul = document.createElement("ul");
 
-    li.textContent = user.fname + ', ' + user.lname;
+  console.log(container);
+
+  users.forEach(function (user) {
+    let li = document.createElement("li");
+
+    li.textContent = user.fname + ", " + user.lname;
     ul.appendChild(li);
+  });
+
+  container.appendChild(ul);
 });
